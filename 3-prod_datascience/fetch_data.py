39f55fe20f17dad295576f74f1eb910c2e5645bb
Cansu Kavili <cansukavili@gmail.com>
2024-12-12 14:41:02 +0100

    🍇dvc steps added🍇

--- 3-prod_datascience/fetch_data.py
+++ 3-prod_datascience/fetch_data.py
@@ -8,16 +8,19 @@ from kfp.dsl import (
     Metrics,
 )
 
-@component(base_image='python:3.9', packages_to_install=["feast==0.36.0", "psycopg2>=2.9", "dask[dataframe]", "s3fs", "pandas"])
+@component(base_image='python:3.9', packages_to_install=["dask[dataframe]", "s3fs", "pandas"])
 def fetch_data(
-    dataset: Output[Dataset]
+    dataset: Output[Dataset],
+    cluster_domain: str,
+    username: str
 ):
     """
     Fetches data from URL
     """
     
     import pandas as pd
-    import yaml    
+    import yaml
+    import os
     
     song_properties = pd.read_parquet('https://github.com/rhoai-mlops/jukebox/raw/refs/heads/main/99-data_prep/song_properties.parquet')
     song_rankings = pd.read_parquet('https://github.com/rhoai-mlops/jukebox/raw/refs/heads/main/99-data_prep/song_rankings.parquet')
@@ -26,6 +29,55 @@ def fetch_data(
     
     dataset.path += ".csv"
     dataset.metadata = {"song_properties": "https://github.com/rhoai-mlops/jukebox/raw/refs/heads/main/99-data_prep/song_properties.parquet", "song_rankings": "https://github.com/rhoai-mlops/jukebox/raw/refs/heads/main/99-data_prep/song_rankings.parquet" }
+    data.to_csv(dataset.path, index=False, header=True)
+
+
+@component(base_image='python:3.9', packages_to_install=["dvc[s3]==3.1.0", "dask[dataframe]", "s3fs", "pandas"])
+def fetch_data_from_dvc(
+    dataset: Output[Dataset],
+    cluster_domain: str
+):
+    """
+    Fetches data from DVC
+    """
+    
+    import pandas as pd
+    import yaml
+    import dvc
+    import configparser
+    
+    def run_command(command, cwd=None, env=None):
+        result = subprocess.run(command, shell=True, cwd=cwd, text=True, capture_output=True, env=env)
+        if result.returncode != 0:
+            raise RuntimeError(f"Command failed: {command}\n{result.stderr}")
+        return result.stdout.strip()
+        
+    def read_hash(dvc_file_path):
+        with open(dvc_file_path, 'r') as file:
+            dvc_data = yaml.safe_load(file)
+            md5_hash = dvc_data['outs'][0]['md5']
+        return md5_hash
+
+    current_path = os.environ.get("PATH", "")
+    new_path = f"{current_path}:/.local/bin"
+    os.environ["PATH"] = new_path
+    
+    print("Updated PATH:", os.environ["PATH"])
+
+    namespace = environ.get("namespace").split('-')[0]
+    
+    run_command(f"git clone https://gitea-gitea.{cluster_domain}/{namespace}/jukebox.git")
+    os.chdir("jukebox")
+    run_command("dvc pull")
+
+    config = configparser.ConfigParser()
+    config.read('.dvc/config')
+
+    data = pd.read_parquet("song_properties.parquet")
+    dataset.path += ".csv"
+    dvc_hash = read_hash("song_properties.parquet.dvc")
+    # print({section: dict(config[section]) for section in config.sections()})
+    dataset.metadata = {"DVC training data hash": dvc_hash}
     data.to_csv(dataset.path, index=False, header=True)
 
 
