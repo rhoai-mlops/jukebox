e1927982736aa01a53bf0c0d2f4ecf26682f1801
Cansu Kavili <cansukavili@gmail.com>
2025-01-17 12:01:58 +0100

    😇 first quiz added

--- .dontlookhere/quiz1.py
+++ .dontlookhere/quiz1.py
@@ -1,9 +1,25 @@
 import library
 
-def quiz_about_elephants():
-    options = ['Option 1', 'AAAAAAAAAAAAAAAAAAA😂AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\nBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB🐝', 'Option 3', 'Correct Option']
-    correct_answer_idx = 3
-    description = "What is your favorite choice? "
+def quiz_eda():
+    options = ["To ensure the model performs exactly as expected without any additional testing", "To directly tune hyperparameters for the ML model", "To understand the data, detect patterns, and identify anomalies"]
+    correct_answer_idx = 2
+    description = "Why is EDA important before building a machine learning model?"
+    correct_answer_message = "Correct choice!👏👏👏"
+    incorrect_answer_message = "Incorrect choice. Please try again."
+
+    library.create_dropdown(
+        options = options,
+        correct_answer_idx=correct_answer_idx,
+        description = description,
+        correct_answer_message = correct_answer_message,
+        incorrect_answer_message = incorrect_answer_message,
+    )
+
+
+def quiz_heatmap():
+    options = ["To visualize the popularity of songs over time", "To identify and visualize relationships between numerical features", "To create a playlist based on user preferences."]
+    correct_answer_idx = 1
+    description = "What is the role of a heatmap in EDA?"
     correct_answer_message = "Correct choice! Well done."
     incorrect_answer_message = "Incorrect choice. Please try again."
 
@@ -14,6 +30,7 @@ def quiz_about_elephants():
         correct_answer_message = correct_answer_message,
         incorrect_answer_message = incorrect_answer_message,
     )
+
 
 def quiz_about_numbers():
     options = ["Many", "Few", "None"]
